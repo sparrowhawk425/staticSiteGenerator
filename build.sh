f7c4fa9fe00a99bbf8c927dd881fe4b54d3446d2
sparrowhawk425 <sparrowhawk425@gmail.com>
2026-01-31 13:00:20 -0800

    Adding build.sh and docs/

---
+++ build.sh
@@ -0,0 +1,1 @@
+python3 src/main.py "/sparrowhawk425/staticSiteGenerator/"
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 
 from textnode import TextNode, TextType
 from functions.textnodemanip import markdown_to_html_node, extract_title
@@ -21,7 +22,7 @@ def copy_files_from_directory_r(source, destination):
             os.mkdir(dest_path)
             copy_files_from_directory_r(file_path, dest_path)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(basepath, from_path, template_path, dest_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     from_md = ""
     html_page = ""
@@ -37,6 +38,7 @@ def generate_page(from_path, template_path, dest_path):
     html = markdown_to_html_node(from_md).to_html()
     title = extract_title(from_md)
     html_page = html_page.replace("{{ Title }}", title).replace("{{ Content }}", html)
+    html_page = html_page.replace('href="', f'href="{basepath}').replace('href=', f'href="{basepath}')
     # create the destination file (create directories if they don't exist)
     print(f"Writing to file {dest_path}")
     dest_dir = os.path.dirname(dest_path)
@@ -45,24 +47,27 @@ def generate_page(from_path, template_path, dest_path):
     with open(dest_path, "w") as dest_file:
         dest_file.write(html_page)
 
-def generate_pages_r(dir_path_content, template_path, dest_dir_path):
+def generate_pages_r(basepath, dir_path_content, template_path, dest_dir_path):
     content_files = os.listdir(dir_path_content)
     for content_file in content_files:
         file_path = os.path.join(dir_path_content, content_file)
         if os.path.isfile(file_path):
             print(f"generating content from {file_path}")
-            generate_page(file_path, template_path, os.path.join(dest_dir_path, content_file.replace(".md", ".html")))
+            generate_page(basepath, file_path, template_path, os.path.join(dest_dir_path, content_file.replace(".md", ".html")))
         if os.path.isdir(file_path):
             print(f"destination directory {dest_dir_path}")
             print(f"directory name {content_file}")
             dest_path = os.path.join(dest_dir_path, content_file)
             print(f"Generating directory {dest_path}")
             os.mkdir(dest_path)
-            generate_pages_r(file_path, template_path, dest_path)
+            generate_pages_r(basepath, file_path, template_path, dest_path)
 
 def main():
-    copy_files_from_directory_r("static", "public")
-    generate_pages_r("content", "template.html", "public")
+    args = sys.argv
+    basepath = "/" if len(args) < 2 else args[1]
+
+    copy_files_from_directory_r("static", "docs")
+    generate_pages_r(basepath, "content", "template.html", "docs")
 
 if __name__ == "__main__":
     main()
